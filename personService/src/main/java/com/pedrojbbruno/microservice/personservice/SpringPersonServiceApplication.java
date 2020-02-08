3265b5d66007a01da6a5e2095389eff7576f0ac6
Pedro Jose Berbert Bruno <pedrobbruno@gmail.com>
2020-02-07 22:39:15 -0300

    ajustando url do HATEOAS

--- personService/src/main/java/com/pedrojbbruno/microservice/personservice/SpringPersonServiceApplication.java
+++ personService/src/main/java/com/pedrojbbruno/microservice/personservice/SpringPersonServiceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
 @EnableCaching
@@ -14,4 +16,8 @@ public class SpringPersonServiceApplication {
 		SpringApplication.run(SpringPersonServiceApplication.class, args);
 	}
 
+	@Bean
+	ForwardedHeaderFilter forwardedHeaderFilter() {
+		return new ForwardedHeaderFilter();
+	}
 }
